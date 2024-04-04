58d79691e5a1c98c8ee023767e81a49b2d36b422
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2024-04-04 07:31:17 +0530

    non_rep_char.c

--- Apr_3_Day10/non_rep_char.c
+++ Apr_3_Day10/non_rep_char.c
@@ -1,1 +1,29 @@
+//non repeated caharcter in string
 
+input : akshaya
+output : k s h y
+
+#include<stdio.h>
+#include <string.h>
+int main()
+{
+    char c[150];
+    int i,j=0,n,t=0;
+    printf("enter string ");
+    scanf("%s",c);
+    for(i=0;c[i];i++ )
+    {
+    	t=0;
+     	for(j=0;c[j];j++)
+        {
+        	if(c[i]==c[j])
+        	      t++;
+        	if(t>1)  
+			break;    
+        }
+        if(t==1)
+        {
+          printf("%c ",c[i]);
+        }
+    }
+}
