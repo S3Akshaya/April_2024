0c666d389e12a15e9794c90e04a1ca30e6e77a2a
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2024-04-05 23:04:15 +0530

    two_num_equal.c

--- Apr_4_Day11/2_num_equal.c
+++ Apr_4_Day11/2_num_equal.c
@@ -1,1 +1,16 @@
+// check if 2 num are equal or not without using bitwise operator
+
+#include<stdio.h>
+int main(){
+    int n1,n2;
+    scanf("%d",&n1);
+    scanf("%d",&n2);
+    if(n1-n2==0){
+        printf("numbers are equal");
+    }
+    else{
+        printf("numbers are not equal");
+    }
+}
+  
 
