ca046f51b4d93c326fc62427f2eb5dd62beedeca
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2024-04-05 22:57:53 +0530

    bin_to_dec.c

--- Apr_4_Day11/bin_to_dec.c
+++ Apr_4_Day11/bin_to_dec.c
@@ -1,1 +1,24 @@
+//binary to decimal
 
+input : 110   
+output : 6
+
+reason : 1*2^2   +   1*2^1    +    0*2^0
+      4 + 2 + 0 ====== 6
+
+#include <stdio.h>   
+int main()  
+{  
+    int n,b,d=0,base=1,rem;  
+    printf ("Enter binary number ");  
+    scanf ("%d", &n);
+    b=n; 
+    while(n>0)  
+    {  
+        rem=n%10;   
+        d= d+rem* base;  
+        n/=10; 
+        base*=2;  
+    }  
+    printf ("decimal number is %d \t", d);
+} 
