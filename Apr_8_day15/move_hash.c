4fbc28d6ea6dc4c0ab4cb197552daa5e809beb36
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2024-04-14 14:50:55 +0530

    move_hash.c

--- Apr_8_day15/move_hash.c
+++ Apr_8_day15/move_hash.c
@@ -1,1 +1,38 @@
 
+//You have write a function that accepts, a string which length is “len”,
+the string has some “#”, in it you have to move all the hashes to the front of the string 
+and return the whole string back and print it.
+
+Input:
+Move#Hash#to#Front
+
+Output:
+###MoveHashtoFront
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[1000];
+    scanf("%[^\n]",s);
+    int i,a=0;
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]=='#')
+        {
+            a++;
+        }
+    }
+    for(i=0;i<a;i++)
+    {
+        printf("#");
+    }
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]!='#')
+        {
+            printf("%c",s[i]);
+        }
+    }
+}
