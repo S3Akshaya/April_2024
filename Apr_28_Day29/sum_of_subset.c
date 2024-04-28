7eb611b153deca5a16d31929e78d3aaa3665a24d
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2024-04-28 15:57:29 +0530

    sum_of_subset.c

---
+++ Apr_28_Day29/sum_of_subset.c
@@ -0,0 +1,31 @@
+//Program to print sums of all subsets in an array.
+
+Input : arr[] = {2, 4, 5}
+Output : 0 2 4 5 6 7 9 11
+
+
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("size of the array: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter elements:\n");
+    for(i=0;i<n;i++) 
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<(1<<n);i++)
+    {
+        int sum=0;
+        for(j=0;j<n;j++) 
+        {
+            if(i&(1<<j))
+            {
+                sum+=arr[j];
+            }
+        }
+        printf("%d ",sum);
+    }
+}
