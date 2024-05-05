45357330f6452f7dd620999da28b70d3f1add76d
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2024-05-05 11:06:08 +0530

    Update even_dig_in_num.c

--- Apr_29_day30/even_dig_in_num.c
+++ Apr_29_day30/even_dig_in_num.c
@@ -1,1 +1,20 @@
+//accept a number and all the EVEN digit in that number
+input: 65746
+output: 16 (6+4+6)
+
+#include<stdio.h>
+int main() 
+{
+    int num, digit, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    while (num != 0) {
+        digit = num % 10;
+        if (digit % 2 == 0) { 
+            sum += digit; 
+        }
+        num /= 10; 
+    }
+    printf("Sum of even digits in the number: %d\n", sum);
+}
 
