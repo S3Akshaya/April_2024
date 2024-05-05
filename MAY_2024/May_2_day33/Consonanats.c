3ca6526a55820ffa62110a9f2b0d1654062ae08e
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2024-05-05 09:53:57 +0530

    Update Consonanats.c

--- MAY_2024/May_2_day33/Consonanats.c
+++ MAY_2024/May_2_day33/Consonanats.c
@@ -1,1 +1,26 @@
+//accept a string and count number of consonants in it
 
+input: akshaya
+output : 4
+
+#include <stdio.h>
+#include<ctype.h>
+int main()
+{
+    char s[50];
+    scanf("%s",s);
+    int i,count;
+    for(i=0;s[i]!='\0';i++) 
+    {
+        s[i]=tolower(s[i]); 
+    }
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u')
+        {
+            count++;
+        }
+        
+    }
+    printf("%d",count);
+}
