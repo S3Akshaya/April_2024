1baf013afd319b0638ac0a41211280f2972a416e
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2024-05-05 10:15:21 +0530

    Create string_rotation.c

---
+++ MAY_2024/May3_Day34/string_rotation.c
@@ -0,0 +1,29 @@
+//Rotate string n times in clockwise direction
+
+input:  
+   string: akshaya
+   no. of rotation: 3
+
+output:
+  yaaksha
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int n,i,j;
+    printf("Enter string: ");
+    scanf("%s", str);
+    printf("number of rotations: ");
+    scanf("%d",&n);
+    int length=strlen(str);
+    n %= length;
+    for(i=0;i<n;i++) {
+        char temp=str[length - 1];
+        for(j=length - 1;j>0; j--) {
+            str[j]=str[j - 1];
+        }
+        str[0] = temp;
+    }
+    printf("Rotated string: %s\n", str);
+}
