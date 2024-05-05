c76ef475324b8651c0229ecf6b859451ef1bf7cf
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2024-05-05 09:51:03 +0530

    Update vowels.c

--- MAY_2024/May_2_day33/vowels.c
+++ MAY_2024/May_2_day33/vowels.c
@@ -1,1 +1,28 @@
+//accwpt a string and count number of vowels in it
+
+input : akshaya
+output : 3
+
+
+#include <stdio.h>
+#include<ctype.h>
+int main()
+{
+    char s[50];
+    scanf("%s",s);
+    int i,count;
+    for(i=0;s[i]!='\0';i++) 
+    {
+        s[i]=tolower(s[i]); 
+    }
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+        {
+            count++;
+        }
+        
+    }
+    printf("%d",count);
+}
 
